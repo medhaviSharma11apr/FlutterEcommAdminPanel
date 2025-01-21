5ae0b8daa646a8c74ad59537a65ad2e17a0bdf68
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2025-01-21 16:01:49 +0530

    final commit for paginated data table

--- lib/features/authentication/screen/dashboard/responsivescreens.dashBoard/dashboard_desktop.dart
+++ lib/features/authentication/screen/dashboard/responsivescreens.dashBoard/dashboard_desktop.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:data_table_2/data_table_2.dart';
+import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -14,51 +15,76 @@ class DashBoardDesktopScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
-          child: Obx((() => Theme(
-                data: Theme.of(context).copyWith(
-                    cardTheme: const CardTheme(
-                  color: Colors.white,
-                  elevation: 0,
-                )),
-                child: PaginatedDataTable2(
-                  initialFirstRowIndex: 1,
-                  columnSpacing: 12,
-                  minWidth: 700,
-                  rowsPerPage: 10,
-                  dividerThickness: 0,
-                  horizontalMargin: 12,
-                  dataRowHeight: 56,
-                  showCheckboxColumn: true,
-                  columns: [
-                    const DataColumn2(label: Text("Column1")),
-                    DataColumn2(
-                      label: const Text("Column2"),
-                      onSort: (columnIndex, ascending) {
-                        controller.sortById(columnIndex, ascending);
-                      },
-                    ),
-                    const DataColumn2(label: Text("Column3")),
-                    DataColumn2(
-                      label: const Text("Column4"),
-                      onSort: (columnIndex, ascending) {
-                        controller.sortById(columnIndex, ascending);
-                      },
-                    ),
-                  ],
-                  source: MyData(),
-                  // pagination
-                  showFirstLastButtons: true,
-                  onPageChanged: (value) {},
-                  renderEmptyRowsInTheEnd: true,
-                  onRowsPerPageChanged: (value) {},
+          child: Column(
+            children: [
+              TextFormField(
+                onChanged: (query) => controller.searchQuery(query),
+                controller: controller.searchtextEditingController,
+                decoration: const InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: Icon(
+                      Icons.search,
+                    )),
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+              Obx((() {
+                Visibility(
+                  visible: false,
+                  child: Text(controller.filtereddatalist.length.toString()),
+                );
+                return SizedBox(
+                  height: 600,
+                  child: Theme(
+                    data: Theme.of(context).copyWith(
+                        cardTheme: const CardTheme(
+                      color: Colors.white,
+                      elevation: 0,
+                    )),
+                    child: PaginatedDataTable2(
+                      initialFirstRowIndex: 1,
+                      columnSpacing: 12,
+                      minWidth: 700,
+                      rowsPerPage: 10,
+                      dividerThickness: 0,
+                      horizontalMargin: 12,
+                      dataRowHeight: 56,
+                      showCheckboxColumn: true,
+                      columns: [
+                        const DataColumn2(label: Text("Column1")),
+                        DataColumn2(
+                          label: const Text("Column2"),
+                          onSort: (columnIndex, ascending) {
+                            controller.sortById(columnIndex, ascending);
+                          },
+                        ),
+                        const DataColumn2(label: Text("Column3")),
+                        DataColumn2(
+                          label: const Text("Column4"),
+                          onSort: (columnIndex, ascending) {
+                            controller.sortById(columnIndex, ascending);
+                          },
+                        ),
+                      ],
+                      source: MyData(),
+                      // pagination
+                      showFirstLastButtons: true,
+                      onPageChanged: (value) {},
+                      renderEmptyRowsInTheEnd: true,
+                      onRowsPerPageChanged: (value) {},
 
-                  // Sorting
-                  sortAscending: controller.sortAscending.value,
-                  sortArrowIcon: Icons.line_axis,
-                  sortColumnIndex: controller.sortColumnIndex.value,
-                
-                ),
-              ))),
+                      // Sorting
+                      sortAscending: controller.sortAscending.value,
+                      sortArrowIcon: Icons.line_axis,
+                      sortColumnIndex: controller.sortColumnIndex.value,
+                      // sortArrowBuilder:
+                    ),
+                  ),
+                );
+              })),
+            ],
+          ),
         ),
       ),
     );
@@ -69,7 +95,7 @@ class MyData extends DataTableSource {
   final dashboardcontroller = Get.put(DashBoardController());
   @override
   DataRow? getRow(int index) {
-    final data = dashboardcontroller.datalist[index];
+    final data = dashboardcontroller.filtereddatalist[index];
     return DataRow2(
         onTap: () {
           log("row $index is clicked");
@@ -94,7 +120,7 @@ class MyData extends DataTableSource {
 
   @override
   // TODO: implement rowCount
-  int get rowCount => dashboardcontroller.datalist.length;
+  int get rowCount => dashboardcontroller.filtereddatalist.length;
 
   @override
   // TODO: implement selectedRowCount
@@ -102,6 +128,7 @@ class MyData extends DataTableSource {
 }
 
 class DashBoardController extends GetxController {
+  var filtereddatalist = <Map<String, dynamic>>[].obs;
   var datalist = <Map<String, dynamic>>[].obs;
   RxList<bool> selectedRows =
       <bool>[].obs; // observable list to store selected rows
@@ -110,10 +137,18 @@ class DashBoardController extends GetxController {
       1.obs; // observable for tracking the index of column for sorting
   RxBool sortAscending = true
       .obs; // observable for tracking the sorting order (ascending or descending)
+
+  final searchtextEditingController =
+      TextEditingController(); // controller for handling search item
   @override
   void onInit() {
     // TODO: implement onInit
     fetchDummyData();
+  }
+
+  void searchQuery(String query) {
+    filtereddatalist.assignAll(datalist
+        .where((item) => item['Column 1'].contains(query.toLowerCase())));
   }
 
   void sortById(
@@ -122,17 +157,19 @@ class DashBoardController extends GetxController {
   ) {
     sortAscending.value = ascending;
 
-    datalist.sort(((a, b) {
+    filtereddatalist.sort(((a, b) {
       if (ascending) {
-        return datalist[0]['Column 1']
+        return filtereddatalist[0]['Column 1']
             .toString()
             .toLowerCase()
-            .compareTo(datalist[0]['Column 1'].toString().toLowerCase());
+            .compareTo(
+                filtereddatalist[0]['Column 1'].toString().toLowerCase());
       } else {
-        return datalist[0]['Column 1']
+        return filtereddatalist[0]['Column 1']
             .toString()
             .toLowerCase()
-            .compareTo(datalist[0]['Column 1'].toString().toLowerCase());
+            .compareTo(
+                filtereddatalist[0]['Column 1'].toString().toLowerCase());
       }
     }));
     this.sortColumnIndex.value = sortColumnIndex;
@@ -144,6 +181,14 @@ class DashBoardController extends GetxController {
       (index) => false,
     )); // Initialize selected rows
     datalist.addAll(List.generate(
+        36,
+        (index) => {
+              'Column 1': 'Data ${index + 1} -1',
+              'Column 2': 'Data ${index + 1} -2',
+              'Column 3': 'Data ${index + 1} -3',
+              'Column 4': 'Data ${index + 1} -4',
+            }));
+    filtereddatalist.addAll(List.generate(
         36,
         (index) => {
               'Column 1': 'Data ${index + 1} -1',
