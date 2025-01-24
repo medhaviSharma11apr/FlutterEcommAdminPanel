398c53608b5daa8d66ec60d36a458e57faac78c3
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2025-01-24 12:21:06 +0530

    Media Uploader

--- lib/features/media/screens.media/widgets/folder_dropdown.dart
+++ lib/features/media/screens.media/widgets/folder_dropdown.dart
@@ -1,14 +1,32 @@
-
-
-
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:ecommerce_admin_panel/features/media/controller/media_controller.dart';
+import 'package:ecommerce_admin_panel/utils/constants/enums.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
 class MediaFolderDropDown extends StatelessWidget {
-  const MediaFolderDropDown({super.key});
+  const MediaFolderDropDown({
+    super.key,
+    this.onChanged,
+  });
+
+  final void Function(MediaCategory?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final controller = MediaController.instance;
+    return Obx((() => SizedBox(
+          width: 240,
+          child: DropdownButtonFormField(
+            isExpanded: false,
+            value: controller.selctedpath.value,
+            onChanged: (value) {},
+            items: MediaCategory.values
+                .map((e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e.name.toUpperCase().toString()),
+                    ))
+                .toList(),
+          ),
+        )));
   }
 }
--- lib/features/media/screens.media/widgets/media_uploader.dart
+++ lib/features/media/screens.media/widgets/media_uploader.dart
@@ -2,7 +2,10 @@ import 'dart:developer';
 
 import 'package:ecommerce_admin_panel/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:ecommerce_admin_panel/features/media/controller/media_controller.dart';
+import 'package:ecommerce_admin_panel/features/media/screens.media/widgets/folder_dropdown.dart';
 import 'package:ecommerce_admin_panel/utils/constants/colors.dart';
+import 'package:ecommerce_admin_panel/utils/constants/enums.dart';
+import 'package:ecommerce_admin_panel/utils/constants/image_strings.dart';
 import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dropzone/flutter_dropzone.dart';
@@ -41,14 +44,90 @@ class MediaUploader extends StatelessWidget {
                     onError: (e) => log('Zone error:$e'),
                     onHover: () => log('Zone hover'),
                     onLeave: () => log('Zone Left'),
+                    onDropInvalid: (value) {
+                      log('ZoneInvalid $value');
+                    },
+                    onDropMultiple: ((value) async {
+                      log('Zone multiple$value');
+                    }),
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        TImages.adidasLogo,
+                        width: 50,
+                        height: 50,
+                      ),
+                      const SizedBox(
+                        height: TSizes.spaceBtwItems,
+                      ),
+                      const Text('Drag and drop Images here'),
+                      const SizedBox(
+                        height: TSizes.spaceBtwItems,
+                      ),
+                      OutlinedButton(
+                          onPressed: (() {}), child: const Text('Select Image'))
+                    ],
                   )
                 ],
               ))
             ],
           ),
         ),
+        const SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
 
         // Locally selected images
+        TRoundedContainer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Folders DropDown
+                  Row(
+                    children: [
+                      // folders dropdown
+                      Text(
+                        "Select Folders ",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      const SizedBox(
+                        width: TSizes.spaceBtwItems,
+                      ),
+                      MediaFolderDropDown(
+                        onChanged: ((MediaCategory? newvalue) {
+                          if (newvalue != null) {
+                            controller.selctedpath.value = newvalue;
+                          }
+                        }),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      TextButton(
+                          onPressed: (() {}), child: const Text("Remove All ")),
+                      const SizedBox(
+                        width: TSizes.spaceBtwItems,
+                      ),
+                      SizedBox(
+                        width: TSizes.buttonWidth,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Upload'),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        )
       ],
     );
   }
